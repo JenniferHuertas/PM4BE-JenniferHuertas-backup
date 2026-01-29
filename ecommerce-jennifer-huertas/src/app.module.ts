de397e336cf2a91364df1cf046e45eb2904f9455
Jennifer Huertas <jennifersu2059@gmail.com>
2026-01-29 09:38:30 -0500

    endpoints creados

--- ecommerce-jennifer-huertas/src/app.module.ts
+++ ecommerce-jennifer-huertas/src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware) 
+      .forRoutes('*');         
+  }
+}
